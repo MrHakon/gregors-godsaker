5cea1590b4dc52aa23f9a5871b5667fb2afa0e10
Haakon Rydland <haakon2000@gmail.com>
2025-02-03 22:19:16 +0100

    Oppdatert beskrivelser

--- GregorsGodsaker/Components/Components/InputComponent.cs
+++ GregorsGodsaker/Components/Components/InputComponent.cs
@@ -4,7 +4,9 @@ namespace GregorsGodsaker.Components.Components;
 
 partial class InputComponent : ComponentBase
 {
-    // Disse blir satt i HTML-koden, ved hjelp av @bind-Value, straks brukeren flytter fokus fra input-feltet.
+    /* Disse blir satt i HTML-koden, ved hjelp av @bind-Value, straks brukeren flytter fokus fra input-feltet.
+    Det er bare sånn det virker. Men du kan også bruke en kombinasjon av Value og onchange for å oppnå det samme, eller
+    gjøre noe mer enn å oppdatere en variabel, som å validere noe eller endre en farge et sted */
     private string InputTekst1 { get; set; } = string.Empty;
     private string InputTekst2 { get; set; } = string.Empty;
 }
